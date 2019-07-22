1a23137a3fcad7e6b25bd4b668c06d0c42d207fd
zhuyaliang <15132211195@163.com>
2019-07-22 23:01:38 +0800

    add soft details object

--- src/app-store-details.c
+++ src/app-store-details.c
@@ -18,6 +18,172 @@
 #include "app-store-util.h"
 #include "app-store-details.h"
 
+//G_DEFINE_TYPE (SoftAppDetails,     soft_app_details,  G_TYPE_OBJECT)
+G_DEFINE_TYPE (SoftAppInfo,        soft_app_info,     G_TYPE_OBJECT)
+
+
+const char	*soft_app_info_get_name	(SoftAppInfo *info)
+{
+	return info->soft_name; 
+}
+
+void soft_app_info_set_name (SoftAppInfo *info,
+		                     const char  *name)
+{
+	g_free (info->soft_name);
+    info->soft_name = g_strdup (name);
+
+}
+const char *soft_app_info_get_icon (SoftAppInfo *info)
+{
+
+	return info->icon_name; 
+}
+void soft_app_info_set_icon (SoftAppInfo *info,
+		                     const char  *icon)
+{
+	g_free (info->icon_name);
+    info->icon_name = g_strdup (icon);
+
+}
+const char *soft_app_info_get_comment (SoftAppInfo *info)
+
+{
+	return info->comment; 
+
+}
+void soft_app_info_set_comment (SoftAppInfo *info,
+		                        const char  *comment)
+{
+	g_free (info->comment);
+    info->comment = g_strdup (comment);
+
+}
+float soft_app_info_get_score (SoftAppInfo *info)
+{
+	return info->score; 
+
+}
+void soft_app_info_set_score (SoftAppInfo *info,
+		                      float        score)
+{
+    info->score = score;
+}
+const char *soft_app_info_get_button (SoftAppInfo *info)
+{
+	return info->button_name; 
+
+}
+void soft_app_info_set_button (SoftAppInfo *info,
+		                       const char  *button_name)
+{
+	g_free (info->button_name);
+    info->button_name = g_strdup (button_name);
+}
+const char *soft_app_info_get_screenshot (SoftAppInfo *info)
+{
+	return info->screenshot; 
+}
+void soft_app_info_set_screenshot (SoftAppInfo *info,
+		                           const char  *screenshot_name)
+{
+	g_free (info->screenshot);
+    info->screenshot = g_strdup (screenshot_name);
+}
+const char *soft_app_info_get_explain (SoftAppInfo *info)
+{
+	return info->explain; 
+}
+void soft_app_info_set_explain (SoftAppInfo *info,
+		                        const char  *explain)
+{
+	g_free (info->explain);
+    info->explain = g_strdup (explain);
+}
+const char *soft_app_info_get_version (SoftAppInfo *info)
+{
+	return info->version; 
+}
+void soft_app_info_set_version (SoftAppInfo *info,
+		                        const char  *version)
+{
+	g_free (info->version);
+    info->version = g_strdup (version);
+}
+const char *soft_app_info_get_protocol (SoftAppInfo *info)
+{
+	return info->protocol; 
+}
+
+void soft_app_info_set_protocol (SoftAppInfo *info,
+		                         const char  *protocol)
+{
+	g_free (info->protocol);
+    info->protocol = g_strdup (protocol);
+
+}
+
+const char *soft_app_info_get_source (SoftAppInfo *info)
+{
+	return info->source; 
+}
+void soft_app_info_set_source (SoftAppInfo *info,
+		                       const char  *source)
+{
+	g_free (info->source);
+    info->source = g_strdup (source);
+}
+
+const char *soft_app_info_get_size (SoftAppInfo *info)
+{
+	return info->size; 
+}
+void soft_app_info_set_size (SoftAppInfo *info,
+		                     const char  *size)
+{
+	g_free (info->size);
+    info->size = g_strdup (size);
+}
+static void
+soft_app_info_finalize (GObject *object)
+{
+    SoftAppInfo *info = SOFT_APP_INFO (object);
+
+    g_free (info->soft_name);
+    g_free (info->icon_name);
+    g_free (info->comment);
+    g_free (info->button_name);
+    g_free (info->screenshot);
+    g_free (info->explain);
+    g_free (info->version);
+    g_free (info->protocol);
+    g_free (info->source);
+    g_free (info->size);
+}
+
+static void
+soft_app_info_class_init (SoftAppInfoClass *klass)
+{
+    GObjectClass *object_class = G_OBJECT_CLASS (klass);
+    object_class->finalize = soft_app_info_finalize;
+}
+
+static void
+soft_app_info_init (SoftAppInfo *info)
+{
+
+}
+
+SoftAppInfo *
+soft_app_info_new (const char *name)
+{
+    SoftAppInfo *info;
+    info = g_object_new (SOFT_APP_TYPE_INFO, NULL);
+	info->soft_name = g_strdup(name);
+
+    return SOFT_APP_INFO (info);
+}
+
 GtkWidget *CreateStoreIndividualDetails (SoftAppStore *app)
 {
     GtkWidget *vbox;
--- src/app-store-details.h
+++ src/app-store-details.h
@@ -19,7 +19,139 @@
 #define __APP_STORE_DETAILS_H__
 
 #include "app-store.h"
+ 
+G_BEGIN_DECLS
+#define SOFT_APP_TYPE_DETAILS                (soft_app_details_get_type ())
+#define SOFT_APP_DETAILS(object)             (G_TYPE_CHECK_INSTANCE_CAST ((object),\
+                                              SOFT_APP_TYPE_DETAILS,\
+                                              SoftAppDetails))
+
+#define SOFT_APP_TYPE_INFO                   (soft_app_info_get_type ())
+#define SOFT_APP_INFO(object)                (G_TYPE_CHECK_INSTANCE_CAST ((object),\
+                                              SOFT_APP_TYPE_INFO,\
+                                              SoftAppInfo))
+
+typedef struct SoftAppInfo
+{
+    GObject      parent_instance;
+	char        *soft_name;
+	char        *icon_name;
+	char        *comment;
+	char        *button_name;
+	float       score;
+	char        *screenshot;
+	char        *explain;
+	char        *version;
+	char        *protocol;
+	char        *source;
+	char        *size;
+}SoftAppInfo;
+
+typedef struct SoftAppInfoClass
+{
+    GObjectClass parent_instance_class;
+}SoftAppInfoClass;
+
+typedef struct SoftAppDetails
+{
+    GObject      parent_instance;
+   
+	SoftAppInfo *info;
+    GtkWidget   *soft_image;
+	GtkWidget   *label_name;
+	GtkWidget   *label_comment;
+	GtkWidget   *stars1;
+	GtkWidget   *stars2;
+	GtkWidget   *stars3;
+	GtkWidget   *stars4;
+	GtkWidget   *stars5;
+	GtkWidget   *button;
+	GtkWidget   *screenshot;
+	GtkWidget   *explain;
+	GtkWidget   *title;
+	GtkWidget   *label_version_title;
+	GtkWidget   *label_version;
+	GtkWidget   *label_protocol_title;
+	GtkWidget   *label_protocol;
+	GtkWidget   *label_source_title;
+	GtkWidget   *label_source;
+	GtkWidget   *label_size_title;
+	GtkWidget   *label_size;
+	GtkWidget   *label_discuss_title;
+	GtkWidget   *button_discuss;
+}SoftAppDetails;
+
+typedef struct SoftAppDetailsClass
+{
+    GObjectClass parent_instance_class;
+}SoftAppDetailsClass;
+
+
+GType             soft_app_details_get_type         (void) G_GNUC_CONST;
+
+SoftAppDetails   *soft_app_details_new              (SoftAppInfo *info);
+
+GType             soft_app_info_get_type            (void) G_GNUC_CONST;
+
+SoftAppInfo      *soft_app_info_new                 (const char  *soft_name);
+
+
+const char       *soft_app_info_get_name            (SoftAppInfo *info);
+
+void              soft_app_info_set_name            (SoftAppInfo *info,
+		                                             const char  *name);
+
+const char       *soft_app_info_get_icon            (SoftAppInfo *info);
+
+void              soft_app_info_set_icon            (SoftAppInfo *info,
+		                                             const char  *icon);
+
+const char       *soft_app_info_get_comment         (SoftAppInfo *info);
+
+
+void              soft_app_info_set_comment         (SoftAppInfo *info,
+		                                             const char  *name);
+ 
+float             soft_app_info_get_score           (SoftAppInfo *info);
+
+void              soft_app_info_set_score           (SoftAppInfo *info,
+		                                             float        score);
+
+const char       *soft_app_info_get_button          (SoftAppInfo *info);
+
+void              soft_app_info_set_button          (SoftAppInfo *info,
+		                                             const char  *button_name);
+
+const char       *soft_app_info_get_screenshot      (SoftAppInfo *info);
+
+void              soft_app_info_set_screenshot      (SoftAppInfo *info,
+		                                             const char  *screenshot_name);
+
+const char       *soft_app_info_get_explain         (SoftAppInfo *info);
+
+void              soft_app_info_set_explain         (SoftAppInfo *info,
+		                                             const char  *explain);
+
+const char       *soft_app_info_get_version         (SoftAppInfo *info);
+
+void              soft_app_info_set_version         (SoftAppInfo *info,
+		                                             const char  *version);
+
+const char       *soft_app_info_get_protocol        (SoftAppInfo *info);
+
+void              soft_app_info_set_protocol        (SoftAppInfo *info,
+		                                             const char  *protovol);
+
+const char       *soft_app_info_get_source          (SoftAppInfo *info);
+
+void              soft_app_info_set_source          (SoftAppInfo *info,
+		                                             const char  *source);
+
+const char       *soft_app_info_get_size            (SoftAppInfo *info);
+
+void              soft_app_info_set_size            (SoftAppInfo *info,
+		                                             const char  *size);
 
 GtkWidget *CreateStoreIndividualDetails (SoftAppStore *app);
-
+G_END_DECLS
 #endif
